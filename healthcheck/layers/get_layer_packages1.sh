7cac48b5e64a9f7efb56e2b4f37e89525176008c
samkohlleffel <sam.kohlleffel@gmail.com>
2020-09-28 12:13:40 -0500

    testing config

---
+++ healthcheck/layers/get_layer_packages1.sh
@@ -0,0 +1,6 @@
+export PKG_DIR="python"
+
+rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
+
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+    pip install -r requirements.txt --no-deps -t ${PKG_DIR}
