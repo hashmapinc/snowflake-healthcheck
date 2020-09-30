cae0c09a4c4e45ff2b9d46fcdc742a2263aa08d3
samkohlleffel <sam.kohlleffel@gmail.com>
2020-09-30 10:22:32 -0500

    testing config

--- healthcheck/layers/get_layer_packages.sh
+++ healthcheck/layers/get_layer_packages.sh
@@ -2,7 +2,5 @@ export PKG_DIR="python"
 
 rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
 
-docker run --rm -v $(pwd):/foo lambci/lambda:build-python3.7 pip install -r requirements.txt --no-deps -t ${PKG_DIR}
-
-# docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
-#     pip install -r requirements.txt --no-deps -t ${PKG_DIR}
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+    pip install snowflake-connector-python==2.3.2 -t ${PKG_DIR} \ exit
