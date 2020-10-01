9f448fe07ddee0071d82f89f03dca986d6752583
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-01 10:19:54 -0500

    testing config

--- snowflake_healthcheck/src/App.js
+++ snowflake_healthcheck/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing to See if changes work.
+          Testing to See if automation works.
         </p>
         <a
           className="App-link"
