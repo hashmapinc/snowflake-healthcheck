0a199fe80d6982b9b9bf5a7aa976d12f009b2538
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-05 11:56:27 -0500

    changing React for blog

--- snowflake_healthcheck/src/App.js
+++ snowflake_healthcheck/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing to See if automation still works correctly.
+          React App Hosted on S3.
         </p>
         <a
           className="App-link"
