8d71321b45b515a975288562c052554b1a179eb9
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-06 11:59:14 -0500

    testing with environment variables

--- snowflake_healthcheck/src/App.js
+++ snowflake_healthcheck/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App Hosted on S3.
+          React App With New Environment Variables.
         </p>
         <a
           className="App-link"
