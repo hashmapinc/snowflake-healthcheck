387fc676d76968aa5dcdcbd3c3c00c5c8b3d9d5a
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-07 16:17:22 -0500

    changing app for blog

--- snowflake_healthcheck/src/App.js
+++ snowflake_healthcheck/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App With New Environment Variables.
+          Testing Automation.
         </p>
         <a
           className="App-link"
