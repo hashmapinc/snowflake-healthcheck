e26dbfe5880135d4db1a8c46045fde1b7b47e440
samkohlleffel <sam.kohlleffel@gmail.com>
2020-10-08 11:07:39 -0500

    changing app.js text for blog

--- snowflake_healthcheck/src/App.js
+++ snowflake_healthcheck/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Automation.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
