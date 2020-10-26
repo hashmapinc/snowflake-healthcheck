61cfb3c2bc6fa7b31ac3821d8014dbc0181578b7
Randy Pitcher <randypitcherii@gmail.com>
2020-10-26 16:17:34 -0500

    fixed autoresizing of charts

--- src/Components/GraphTab.js
+++ src/Components/GraphTab.js
@@ -7,8 +7,13 @@ import '../main.css'
 class GraphTab extends Component {
 
     render() {
+      const interval = setInterval(function() {
+        // this is a short term fix to bad plotly resize handling.
+        window.dispatchEvent(new Event('resize'));
+      }, 500);
+
         return (
-            <Tabs defaultActiveKey="Warehouse" className="mt-4">
+            <Tabs defaultActiveKey="Warehouse" className="mt-4" mountOnEnter={true}>
                 <Tab eventKey="Warehouse" title="Warehouse">
                     <WarehouseDashboard warehouse_health_data={this.props.warehouse_health_data} warehouse_usage_data={this.props.warehouse_usage_data}/>
                 </Tab>
